70930f867e76a98df3390200660561c1d60ffb11
Jose Angel <joseangcabb@gmail.com>
2025-09-22 00:15:08 -0500

    chore: add Prettier for code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,24 @@
-import eslint from "@eslint/js";
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
-import eslintPluginAstro from "eslint-plugin-astro";
-import astroParser from "astro-eslint-parser";
-import tseslint from "typescript-eslint";
-import tsParser from "@typescript-eslint/parser";
-import jsxA11y from "eslint-plugin-jsx-a11y";
+import eslint from '@eslint/js'
+import globals from 'globals'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginAstro from 'eslint-plugin-astro'
+import astroParser from 'astro-eslint-parser'
+import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
-  globalIgnores([
-    "dist/**",
-    "node_modules/**",
-    "build/**",
-    ".astro/**",
-    "**/*.d.ts",
-  ]),
+  globalIgnores(['dist/**', 'node_modules/**', 'build/**', '.astro/**', '**/*.d.ts']),
 
   // General language settings for the linter
   {
     languageOptions: {
       globals: { ...globals.node, ...globals.browser },
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        project: "./tsconfig.json",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
       },
     },
   },
@@ -33,32 +28,32 @@ export default defineConfig([
 
   // Astro
   {
-    files: ["**/*.astro"],
+    files: ['**/*.astro'],
     extends: [
       tseslint.configs.recommended,
       eslintPluginAstro.configs.recommended,
-      eslintPluginAstro.configs["jsx-a11y-strict"],
+      eslintPluginAstro.configs['jsx-a11y-strict'],
     ],
     languageOptions: {
       parser: astroParser,
       parserOptions: {
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
         parser: tsParser,
-        extraFileExtensions: [".astro"],
+        extraFileExtensions: ['.astro'],
       },
     },
   },
 
   // TypeScript
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
     extends: [tseslint.configs.recommended],
     languageOptions: { parser: tsParser },
   },
 
   // JSX (React components)
   {
-    files: ["**/*.jsx"],
+    files: ['**/*.jsx'],
     extends: [jsxA11y.configs.strict],
     languageOptions: {
       parserOptions: {
@@ -71,7 +66,7 @@ export default defineConfig([
 
   // TypeScript JSX (React components)
   {
-    files: ["**/*.tsx"],
+    files: ['**/*.tsx'],
     extends: [tseslint.configs.recommended, jsxA11y.configs.strict],
     languageOptions: {
       parser: tsParser,
@@ -82,4 +77,7 @@ export default defineConfig([
       },
     },
   },
-]);
+
+  // Prettier
+  eslintPluginPrettierRecommended,
+])
