2fb984c01985a48100952ad2f343a70ef2de627a
Jose Angel <joseangcabb@gmail.com>
2025-09-22 01:45:38 -0500

    chore(config): set site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://joseangcabb.github.io',
+  site: 'https://josepalacioscabb.dev',
   vite: {
     plugins: [tailwindcss()],
   },
