3bbf7e4c2ea881bf34578c1c5249bf10fb935e07
Jose Angel <joseangcabb@gmail.com>
2025-09-29 15:17:38 -0500

    chore: add @astrojs/mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import mdx from '@astrojs/mdx'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://josepalacioscabb.dev',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 })
