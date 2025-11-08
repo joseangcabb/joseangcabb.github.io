b65a5f52eda0d32bd5d7186a6bf355dd0e775ef5
Jose Angel <joseangcabb@gmail.com>
2025-11-08 14:06:25 -0500

    feat(config): add global siteConfig

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: 'Jose Palacios',
+  navItems: [
+    { label: 'About', href: '/', variant: 'link' },
+    { label: 'Skills', href: '#skills', variant: 'link' },
+    { label: 'Experiences', href: '#experiences', variant: 'link' },
+    { label: 'Blog', href: '/blog', variant: 'button' },
+  ],
+}
